130c7a367f17cd8cda90fbc121f9fe46537991af
TazoKvatani <127935058+TazoKvatani@users.noreply.github.com>
2023-03-15 12:36:39 +0400

    Java task 2

--- Main.java
+++ Main.java
@@ -1,26 +1,20 @@
 public class Main {
-    // method to find unique int in array where all ints are duplicated except one
-    static int singleNumber(int[] nums){
-        int singleNum=0;  //variable which will hold unique int
-        int counter; //counter variable
-        for (int num : nums) { //looping through ints
-            counter = 0;//reset counter to zero
-            for (int i : nums) {//looping through array to compare all ints in array with current int
-                if (num == i) {//if matching int for current int is found counter is increased
-                    counter++;
-                }
-            }
-            if (counter == 1) { //if counter is 1 it means current int only has matched once.it does not have duplicate in array
-                singleNum = num;   //set singleNum to current int
+    static int minSplit(int amount){//method to split your amount of money into minimum amount of coins
+        int[] coins={50,20,10,5,1};//array for available coins
+        int minAmountOfCoins=0;//assign amount of coins as 0
+        int counter=0;//counter for iterating through array
+        while(amount!=0){
+            if(amount>=coins[counter]){//check if coin can be subtracted from amount of money without being less than zero
+                amount=amount-coins[counter];//updating amount of money
+                minAmountOfCoins++; //increment minAmountOfCoins when a coin is used
+            } else {
+                counter++; //move to the next coin if the current coin is too large
             }
         }
-        return singleNum;//return unique int
+        return minAmountOfCoins;//returning amount of coins
     }
-
-
     public static void main(String[] args) {
-        int[] arr={1,2,11,1,2,3,3,4,4,5,5,6,6};//example array
-        System.out.println(singleNumber(arr));//print unique array
-
+        System.out.println(minSplit(31));//printing value
+        System.out.println(minSplit(55));//printing value
     }
 }
